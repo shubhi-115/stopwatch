bb81e1da6cdcf7e273ed373eb0c90a36377a7c7d
ShubhiAgarwal115 <shubhiagarwal8989@gmail.com>
2021-07-22 00:27:07 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import DisplayComponent from './components/DisplayComponent';
+import BtnComponent from './components/BtnComponent';
 import './App.css';
 
 function App() {
+  const [time, setTime] = useState({ms:0, s:0, m:0, h:0});
+  const [interv, setInterv] = useState();
+  const [status, setStatus] = useState(0);
+  // Not started = 0
+  // started = 1
+  // stopped = 2
+
+  const start = () => {
+    run();
+    setStatus(1);
+    setInterv(setInterval(run, 10));
+  };
+
+  var updatedMs = time.ms, updatedS = time.s, updatedM = time.m, updatedH = time.h;
+
+  const run = () => {
+    if(updatedM === 60){
+      updatedH++;
+      updatedM = 0;
+    }
+    if(updatedS === 60){
+      updatedM++;
+      updatedS = 0;
+    }
+    if(updatedMs === 100){
+      updatedS++;
+      updatedMs = 0;
+    }
+    updatedMs++;
+    return setTime({ms:updatedMs, s:updatedS, m:updatedM, h:updatedH});
+  };
+
+  const stop = () => {
+    clearInterval(interv);
+    setStatus(2);
+  };
+
+  const reset = () => {
+    clearInterval(interv);
+    setStatus(0);
+    setTime({ms:0, s:0, m:0, h:0})
+  };
+
+  const resume = () => start();
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-section">
+     <div className="clock-holder">
+          <div className="stopwatch">
+               <DisplayComponent time={time}/>
+               <BtnComponent status={status} resume={resume} reset={reset} stop={stop} start={start}/>
+          </div>
+     </div>
     </div>
   );
 }
